464a83b1d8e9fca1cf9f5d0f631a01f3efea7298
rsabe004 <rsabe004@ucr.edu>
2021-11-14 20:44:02 -0800

    Moving main() from Menu.cpp to Main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+/*int main() {
     //unique id test
     static int idCounter = 0;
     
@@ -19,4 +19,44 @@ int main() {
     cout << endl << test4->getID();
 
     //delete test
+}*/
+
+int main(){
+
+char userInput;
+
+        while((userInput != "q") || (userInput != "Q")){
+                displayMenu();
+
+
+                cin >> userInput;
+
+                while(userInput != "A" && userInput != "B" && userInput != "C"
+                        userInput != "D" && userInput != "E" && userInput != "F" && userInput != "Q"){
+
+                                cout << "Please enter a valid input." <<endl;
+                                cout << displayMenu();
+                                cin >> userInput;
+                }
+
+                if (userInput == "a" || userInput == "A"){
+                        addList();
+                }
+                if (userInput == "b" || userInput == "B"){
+                        removeList();
+                }
+                if (userInput == "c" || userInput == "C"){
+                        addTask();
+                }
+                if (userInput == "d" || userInput == "D"){
+                        removeTask();
+                }
+                if (userInput == "e" || userInput == "E"){
+                        displayList();
+                }
+                if (userInput == "f" || userInput == "F"){
+                        displayTask();
+                }
+}
+return 0;
 }
